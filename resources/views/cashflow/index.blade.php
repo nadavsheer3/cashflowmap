042a901dd0a4ef7a487a1e5b0dd5d0245c5c8fd3
Nadav Sheer <nadavsheer@gmail.com>
2022-04-13 13:50:59 +0300

    fixed flow values

--- resources/views/cashflow/index.blade.php
+++ resources/views/cashflow/index.blade.php
@@ -1,104 +1,104 @@
 <x-app-layout>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Cashflow') }}
-        </h2>
-    </x-slot>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+			{{ __('Cashflow') }}
+		</h2>
+	</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+	<div class="py-12">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-            	<!-- Success message -->
-        		<x-success-status class="mb-4" :status="session('message')" />
+				<!-- Success message -->
+				<x-success-status class="mb-4" :status="session('message')" />
 
 				<!-- Validation Errors -->
-        		<x-validation-errors class="mb-4" :errors="$errors" />
+				<x-validation-errors class="mb-4" :errors="$errors" />
 
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{ url('cashflow') }}" method="POST">
+				<div class="p-6 bg-white border-b border-gray-200">
+					<form action="{{ url('cashflow') }}" method="POST">
 
-                    	@csrf
+						@csrf
 
-                    	<div class="flex justify-between">
-			            <div class="py-2">
-			            	<div class="flex">
-			                	<x-input id="flow" type="radio" name="flow" :value="old('Income')" required autofocus />
-			                	<x-label for="flow" :value="__('Income')" />
-			                </div>
-			                <div class="flex">
-			                	<x-input id="flow" type="radio" name="flow" :value="old('Expence')" required autofocus />
-			                	<x-label for="flow" :value="__('Expence')" />
-			                </div>
-			            </div>
+						<div class="flex justify-between">
+							<div class="py-2">
+								<div>
+									<x-label for="flow" :value="__('Flow')" />
+									<x-input name="flow" list="flow" required autofocus />
+									<datalist id="flow">
+										<option value="Income">
+										<option value="Expence">
+									</datalist>
+								</div>
+							</div>
 
-			            <div class="px-2">
-			                <x-label for="category" :value="__('Category')" />
+							<div class="px-2">
+								<x-label for="category" :value="__('Category')" />
 
-			                <x-input id="category" class="block mt-1" type="text" name="category" :value="old('category')" required autofocus />
-			            </div>
+								<x-input id="category" class="block mt-1" type="text" name="category" :value="old('category')" required autofocus />
+							</div>
 
-			            <div class="px-2">
-			                <x-label for="name" :value="__('Name')" />
+							<div class="px-2">
+								<x-label for="name" :value="__('Name')" />
 
-			                <x-input id="name" class="block mt-1" type="text" name="name" :value="old('name')" required autofocus />
-			            </div>
-			            
-			            <div class="px-2">
-			                <x-label for="amount" :value="__('Amount')" />
+								<x-input id="name" class="block mt-1" type="text" name="name" :value="old('name')" required autofocus />
+							</div>
 
-			                <x-input id="amount" class="block mt-1" type="number" name="amount" :value="old('amount')" required autofocus />
-			            </div>
+							<div class="px-2">
+								<x-label for="amount" :value="__('Amount')" />
 
-			            <x-button class="ml-3">
-                    		{{ __('Insert') }}
-                		</x-button>
-			            </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+								<x-input id="amount" class="block mt-1" type="number" name="amount" :value="old('amount')" required autofocus />
+							</div>
 
-    <div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+							<x-button class="ml-3">
+								{{ __('Insert') }}
+							</x-button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 
-                	<table class="table table-bordered">
-                		<thead>
-                			<tr>
-	                			<th>Flow</th>
-    	            			<th>Category</th>
-        	        			<th>Name</th>
-            	    			<th>Amount</th>
-                			</tr>
-                		</thead>
-                		<tbody>
-                			@forelse ($info as $row)
-                			<tr>
-                				<td>{{ $row->flow }}</td>
-    	            			<td>{{ $row->category }}</td>
-        	        			<td>{{ $row->name }}</td>
-            	    			<td>{{ $row->amount }}</td>
-                			</tr>
-                			@empty
-	               			<tr>
-                				<td colspan="6">No records Found...</td>
-                			</tr>
-                			@endforelse
-                			<tr>
-                				<td>Total</td>
-    	            			<td>+</td>
-        	        			<td>-</td>
-            	    			<td>flow</td>
-                			</tr>
-                		</tbody>
-                	</table>
+	<div>
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+				<div class="p-6 bg-white border-b border-gray-200">
 
-                </div>
-	        </div>
-        </div>
-    </div>                	
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+								<th>Flow</th>
+								<th>Category</th>
+								<th>Name</th>
+								<th>Amount</th>
+							</tr>
+						</thead>
+						<tbody>
+							@forelse ($info as $row)
+							<tr>
+								<td>{{ $row->flow }}</td>
+								<td>{{ $row->category }}</td>
+								<td>{{ $row->name }}</td>
+								<td>{{ $row->amount }}</td>
+							</tr>
+							@empty
+							<tr>
+								<td colspan="6">No records Found...</td>
+							</tr>
+							@endforelse
+							<tr>
+								<td>Total</td>
+								<td>Income($)</td>
+								<td>Expence(-$)</td>
+								<td>Income-Expence</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>                	
+
 </x-app-layout>
